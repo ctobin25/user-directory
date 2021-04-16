1d6890697059876dcf61e951bb7ec73bd294608b
Caroline Tobin <ctobin25>
2021-04-16 19:00:53 -0400

    table header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 
-import { useEffect, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import API from "./util/API";
 
 function App() {
+
+  const [employeeState, setEmployeeState] = useState([])
+
+
     useEffect(() => {
       API.searchEmployee() 
       .then(res => {
        console.log('res',res)
+       setEmployeeState(res.data.results)
+
       })
     }, [])
+
+    console.log(employeeState)
 
 
 
   return (
     <div className="App">
      <h1> user directory </h1>
-
+        {employeeState.map(employee => {
+          return(
+            <>
+                     <p>{employee.name.first}</p>
+          <p>{employee.name.last}</p>
+            </>
+          )
+        })}
     </div>
   );
 }
